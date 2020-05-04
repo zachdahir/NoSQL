7b6aeff3266e03ee6442c9612250bdef06d18f98
Zach Dahir <zachdahir@yahoo.com>
2020-05-03 21:00:18 -0500

    Adding week-1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-335
 Bellevue University Introduction to NoSQL
+
+# contributors
+Professor Krasso - Bellevue University
+Zachary Dahir - Bellevue University
---
+++ week-1/dahir-assignment-1.4.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Zachary Dahir
+; Date: 5/02/20/2020
+; Description: Data Models
+;===========================================
+*/
+
+{
+    "user": {
+
+        "roles":[
+            "Manager",
+            "Quality Assurance"
+        ],
+
+        "dependents": [
+            "Sarah",
+            "Josh",
+            "Jake"
+        ],
+        
+        "birthdate": "10/15/1996"
+    }
+};
