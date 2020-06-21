6db6be44ea5b32da191a7fcd180554d6d04cddbe
Zach Dahir <zachdahir@yahoo.com>
2020-06-21 15:59:53 -0500

    adding week 8

---
+++ week-8/dahir_calculator.py
@@ -0,0 +1,25 @@
+#
+#============================================
+# Title: 8.3
+# Author: Zachary Dahir
+# Date: 6-15-20
+# Description: Calculator
+#===========================================
+#
+
+def add(x, y):
+    print(x + y)
+
+def subtract(x, y):
+    print(x - y)
+
+def multiply(x, y):
+    print(x * y)
+
+def divide(x, y):
+    print(x / y)
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(multiply(5, 6))
+print(divide(8, 2))
---
+++ week-8/dahir_hello_world.py
@@ -0,0 +1,9 @@
+#
+#============================================
+# Title: 8.2
+# Author: Zachary Dahir
+# Date: 6-15-20
+# Description: Hello World
+#===========================================
+#
+print('Hello World')
